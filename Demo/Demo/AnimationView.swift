f010b7b8663c50bf458a1cd133e14f1a28ca05fc
zuopanYao <yaozuopan@icloud.com>
2023-11-11 10:13:26 +0800

    tmp

--- Demo/Demo/AnimationView.swift
+++ Demo/Demo/AnimationView.swift
@@ -5,4 +5,57 @@
 //  Created by Harvey on 2023/11/11.
 //
 
-import Foundation
+import UIKit
+import BasicAnimation
+
+class AnimationBaseView: UIView {
+    
+    deinit {
+        print("deinit \(Self.self)")
+
+    }
+}
+
+class AnimationView: AnimationBaseView {
+    
+    lazy var contentView: UIView = {
+        let view = UIView(frame: CGRect.init(x: 60, y: 200, width: 200, height: 200))
+        view.backgroundColor = .blue
+        view.layer.position = self.center
+        return view
+    }()
+    
+    init() {
+        super.init(frame: UIScreen.main.bounds)
+        addSubview(contentView)
+        
+        addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(close(_:))))
+    }
+    
+    func show() {
+        UIApplication.shared.delegate?.window??.addSubview(self)
+        
+        let scaleAnimtion = BAAnimation.scale(from: 0.5, to: 1.0)
+       
+        let controller = contentView.ba
+            .create(animation: scaleAnimtion)
+        controller.fillMode(fillMode: .both).run()
+        
+        let mySpring = BASpring(3.0, mass: 1.0, stiffness: 50.0, damping: 5.0)
+        let animation = BAAnimation.position(from: CGPoint(x: contentView.center.x, y: contentView.center.y - 200), to:center)
+        contentView.ba.create(animation: animation, spring: mySpring).fillMode(fillMode: .both).run()
+        
+    }
+    
+    @objc func close(_ tap: UITapGestureRecognizer) {
+        removeFromSuperview()
+    }
+    
+//    deinit {
+//        print("deinit \(Self.self)")
+//    }
+//    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -11,7 +11,7 @@ import BasicAnimation
 class ViewController: UIViewController {
     
     var aView: UIView!
-    var bView: UIView!
+    var myView: UIView!
     var cView: UIView!
     var dView: UIView!
 
@@ -19,55 +19,80 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        aView = UIView(frame: CGRect(x: 100, y: 80, width: 180, height: 80))
-        aView.backgroundColor = UIColor.red
-        view.addSubview(aView)
-        
-        bView = UIView(frame: CGRect(x: 100, y: 230, width: 120, height: 120))
-        bView.backgroundColor = UIColor.blue
-        view.addSubview(bView)
-
-        
-        cView = UIView(frame: CGRect(x: 130, y: 400, width: 120, height: 120))
-        cView.backgroundColor = UIColor.purple
-        cView.layer.shadowOffset = .zero
-        view.addSubview(cView)
-        
-        dView = UIView(frame: CGRect(x: 0, y: 00, width: 120, height: 120))
-        dView.backgroundColor = UIColor.yellow
-        view.addSubview(dView)
+//        aView = UIView(frame: CGRect(x: 100, y: 80, width: 180, height: 80))
+//        aView.backgroundColor = UIColor.red
+//        view.addSubview(aView)
+//        
+//        myView = UIView(frame: CGRect(x: 100, y: 230, width: 120, height: 120))
+//        myView.backgroundColor = UIColor.blue
+//        view.addSubview(myView)
+//
+//        
+//        cView = UIView(frame: CGRect(x: 130, y: 400, width: 120, height: 120))
+//        cView.backgroundColor = UIColor.purple
+//        cView.layer.shadowOffset = .zero
+//        view.addSubview(cView)
+//        
+//        dView = UIView(frame: CGRect(x: 0, y: 00, width: 120, height: 120))
+//        dView.backgroundColor = UIColor.yellow
+//        view.addSubview(dView)
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        let btn = UIButton(type: .custom)
+        btn.setTitle("Scale", for: .normal)
+        btn.setTitleColor(.red, for: .normal)
+        btn.frame = view.bounds
+        view.addSubview(btn)
+        
+        btn.addTarget(self, action: #selector(dodo), for: .touchUpInside)
+        
+//        let scaleAnimtion = BAAnimation.scale(from: CGPoint(x: 1.0, y: 1.0), to: CGPoint(x: 0.4, y: 0.5))
+//        let controller = aView.ba.create(animation: scaleAnimtion)
+//        controller.delay(2.0).run()
         
         /// 简单动画
-        aView.ba.create(animation: .scale(from: CGPoint(x: 1.0, y: 1.0), to: CGPoint(x: 0.4, y: 0.5))).delay(2.0).run()
+//        aView.ba.create(animation: .scale(from: CGPoint(x: 1.0, y: 1.0), to: CGPoint(x: 0.4, y: 0.5))).delay(2.0).run()
 
+        
+        
         /// 自定义动画
-        bView.ba.create(animation: .rotationZ(from: 0, to: Float.pi * 2.0)).duration(1.0).repeatCount(999).timingCurve(.linear).run()
+//        myView.ba.create(animation: .rotationZ(from: 0, to: Float.pi * 2.0)).duration(1.0).repeatCount(999).timingCurve(.linear).run()
+        
+        
+//        let mySpring = BASpring(10.0, mass: 2.0, stiffness: 100.0, damping: 5.0)
+//        let animation = BAAnimation.position(from: myView.center, to:CGPoint(x: 200, y: 200))
+//        myView.ba.create(animation: animation, spring: mySpring).run()
+//        
+        
+      
 
-        /// 动画组 1
-        let animation1 = BAAnimation.shadowColor(from: UIColor.black.cgColor, to: UIColor.green.cgColor).create()
-        let animation2 = BAAnimation.shadowRadius(from: 0.0, to: 50.0).create()
-        let animation3 = BAAnimation.shadowOpacity(from: 0.2, to: 0.8).create()
-
-        cView.ba.create(animation: [animation1, animation2, animation3]).delay(3.0).duration(2.0).repeatCount(9999).isReverses(true).run()
-
-        /// 动画组 2
-        let animation4 = BAAnimation.rotationZ(from: 0, to: Float.pi * 2.0).create()
-        let animation5 = BAAnimation.position(from: dView.center, to: CGPoint(x: 300, y: 400)).create()
-
-        let groupAnimation = dView.ba.create(animation: [animation4, animation5])
-
-        groupAnimation.delay(3.0)
-            .duration(2.0)
-            .timingCurve(.easeInEaseOut)
-            .repeatCount(9999)
-            .isReverses(true)
-            .run()
+//        /// 动画组 1
+//        let animation1 = BAAnimation.shadowColor(from: UIColor.black.cgColor, to: UIColor.green.cgColor).create()
+//        let animation2 = BAAnimation.shadowRadius(from: 0.0, to: 50.0).create()
+//        let animation3 = BAAnimation.shadowOpacity(from: 0.2, to: 0.8).create()
+//
+//        cView.ba.create(animation: [animation1, animation2, animation3]).delay(3.0).duration(2.0).repeatCount(9999).isReverses(true).run()
+//
+//        /// 动画组 2
+//        let animation4 = BAAnimation.rotationZ(from: 0, to: Float.pi * 2.0).create()
+//        let animation5 = BAAnimation.position(from: dView.center, to: CGPoint(x: 300, y: 400)).create()
+//
+//        let groupAnimation = dView.ba.create(animation: [animation4, animation5])
+//
+//        groupAnimation.delay(3.0)
+//            .duration(2.0)
+//            .timingCurve(.easeInEaseOut)
+//            .repeatCount(9999)
+//            .isReverses(true)
+//            .run()
         
         // dView.ba.removeAnimation(animation: groupAnimation)
+    }
+    
+    @objc func dodo() {
+        AnimationView().show()
     }
 }
 
